13b3a5dac2eb3766ea077ae3f226f61f08b0a4cb
Wei Ouyang <oeway007@gmail.com>
2020-06-30 23:45:53 +0200

    upgrade format to support links (#6)

--- src/compile_model_manifest.py
+++ src/compile_model_manifest.py
@@ -7,23 +7,27 @@ from pathlib import Path
 
 compiled_models = []
 preserved_keys = [
-    "source",
-    "model",
-    "weights",
-    "files",
-    "applications",
-    "download_url",
-    "id",
-    "version",
-    "format_version",
     "api_version",
-    "name",
-    "description",
-    "cite",
     "authors",
-    "documentation",
-    "tags",
+    "git_repo",
+    "cite",
+    "co2",
     "covers",
+    "badges",
+    "description",
+    "documentation",
+    "download_url",
+    "attachments",
+    "format_version",
+    "id",
+    "license",
+    "model",
+    "name",
+    "source",
+    "tags",
+    "version",
+    "links",
+    "icon",
 ]
 assert "url" not in preserved_keys
 
@@ -31,45 +35,80 @@ models_yaml_file = Path(__file__).parent / "manifest.bioimage.io.yaml"
 models_yaml = yaml.safe_load(models_yaml_file.read_text())
 
 
-
 compiled_apps = []
 apps_names = []
-for item in models_yaml["applications"]:
+for item in models_yaml["application"]:
     app_url = item["source"]
-    if not app_url.startswith("http"):
-        app_url = item["source"].strip("/").strip("./")
-        app_url = models_yaml["url_root"].strip("/") + "/" + app_url
+    if os.path.exists(app_url):
+        content = open(app_url, "r").read()
+        if not app_url.startswith("http"):
+            app_url = item["source"].strip("/").strip("./")
+            app_url = models_yaml["url_root"].strip("/") + "/" + app_url
+    else:
+        if not app_url.startswith("http"):
+            app_url = item["source"].strip("/").strip("./")
+            app_url = models_yaml["url_root"].strip("/") + "/" + app_url
 
-    response = requests.get(app_url)
-    if response.status_code != 200:
-        print("Failed to fetch model config from " + app_url)
-        continue
-    
-    content = response.content.decode('utf-8')
-    found = re.findall('<config (.*)>(.*)</config>', content, re.DOTALL)[0]
-    if 'json' in found[0]:
+        response = requests.get(app_url)
+        if response.status_code != 200:
+            print("Failed to fetch model config from " + app_url)
+            continue
+
+        content = response.content.decode("utf-8")
+    found = re.findall("<config (.*)>(.*)</config>", content, re.DOTALL)[0]
+    if "json" in found[0]:
         plugin_config = json.loads(found[1])
-    elif 'yaml' in found[0]:
+    elif "yaml" in found[0]:
         plugin_config = yaml.safe_load(found[1])
     else:
         raise Exception("config not found in " + app_url)
 
     app_config = {"id": item["id"], "type": "application", "source": app_url}
-    fields = ["icon", "name", "version", "api_version", "description", "license", "requirements", "dependencies", "env"]
+    fields = [
+        "icon",
+        "name",
+        "version",
+        "api_version",
+        "description",
+        "license",
+        "requirements",
+        "dependencies",
+        "env",
+    ]
     for f in fields:
         if f in plugin_config:
             app_config[f] = plugin_config[f]
-    tags = plugin_config.get('labels', []) + plugin_config.get('flags', [])
-    app_config['tags'] = tags
-    app_config['covers'] = plugin_config.get('cover', [])
-    app_config['authors'] = plugin_config.get('author', [])
-    assert item["id"] == plugin_config["name"], "Please use the app name (" + plugin_config["name"] + ") as its application id."
+    tags = plugin_config.get("labels", []) + plugin_config.get("flags", [])
+    app_config["tags"] = tags
+
+    app_config["covers"] = plugin_config.get("cover")
+    # make sure we have a list
+    if not app_config["covers"]:
+        app_config["covers"] = []
+    elif type(app_config["covers"]) is not list:
+        app_config["covers"] = [app_config["covers"]]
+
+    app_config["badges"] = plugin_config.get("badge")
+    if not app_config["badges"]:
+        app_config["badges"] = []
+    elif type(app_config["badges"]) is not list:
+        app_config["badges"] = [app_config["badges"]]
+
+    app_config["authors"] = plugin_config.get("author")
+    if not app_config["authors"]:
+        app_config["authors"] = []
+    elif type(app_config["authors"]) is not list:
+        app_config["authors"] = [app_config["authors"]]
+
+    assert item["id"] == plugin_config["name"], (
+        "Please use the app name (" + plugin_config["name"] + ") as its application id."
+    )
     apps_names.append(plugin_config["name"])
     compiled_apps.append(app_config)
 
-for tp in ["models", "datasets", "notebooks"]:
+for tp in ["model", "dataset", "notebook"]:
     for item in models_yaml[tp]:
-        
+
         source = item["source"]
         root_url = "/".join(source.split("/")[:-1])
         response = requests.get(source)
@@ -81,7 +120,12 @@ for tp in ["models", "datasets", "notebooks"]:
 
         # merge item from models.yaml to model config
         model_config.update(item)
-        model_info = {"root_url": root_url, "type": tp[:-1]} # remove `s`
+        model_info = {"root_url": root_url, "type": tp, "attachments": {}}
+        attachments = model_info["attachments"]
+        if "files" in model_config:
+            attachments["files"] = model_config["files"]
+        if "weights" in model_config:
+            attachments["weights"] = model_config["weights"]
         for k in model_config:
             # normalize relative path
             if k in ["documentation"]:
@@ -94,11 +138,9 @@ for tp in ["models", "datasets", "notebooks"]:
 
             if k in preserved_keys:
                 model_info[k] = model_config[k]
-        apps = model_info.get("applications")
-        if apps is not None and len(apps) > 0:
-            for app in apps:
-                assert app in apps_names, "{} not found in the application list, please make sure you use the correct application name.".format(app)
 
+        if len(model_info["attachments"]) <= 0:
+            del model_info["attachments"]
 
         compiled_models.append(model_info)
         compiled_models.sort(key=lambda m: m["name"], reverse=True)
